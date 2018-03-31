2f92a1eec2f094492da4f266337a5c482db65798
Toby Wilkins <tobwilk@gmail.com>
2018-03-31 22:33:57 +0100

    moved print statements out of the get method

--- cleeve-weather-monitor.py
+++ cleeve-weather-monitor.py
@@ -16,6 +16,7 @@ class CleeveWeatherMonitor:
         # width property. Not nice, but it works.
 
         count = 0
+        json_data = {}
 
         # Find and get the weather table
         page = requests.get(SCRAPE_URL).text
@@ -41,74 +42,74 @@ class CleeveWeatherMonitor:
         soup = BeautifulSoup(str(row_time), 'html.parser')
         td = soup.find_all('td')
         time = td[0].text
+        json_data['time'] = time
 
         #PRESSURE
         soup = BeautifulSoup(str(row_current_pressure), 'html.parser')
         td = soup.find_all('td')
         current_pressure = td[0].text
+        json_data['current_pressure'] = current_pressure
 
         #WIND
         soup = BeautifulSoup(str(row_current_wind), 'html.parser')
         td = soup.find_all('td')
         current_wind = td[0].text
+        json_data['current_wind'] = current_wind
 
         # RAIN TODAY
 
         soup = BeautifulSoup(str(row_rain_today), 'html.parser')
         td = soup.find_all('td')
         rain_today = td[0].text
+        json_data['rain_today'] = rain_today
 
         # RAIN RECENTLY
 
         soup = BeautifulSoup(str(row_rain_recent), 'html.parser')
         td = soup.find_all('td')
         rain_recent = td[0].text
+        json_data['rain_recent'] = rain_recent
 
         # TEMP
 
         soup = BeautifulSoup(str(row_temp_current), 'html.parser')
         td = soup.find_all('td')
         temp_current = td[1].text
+        json_data['temp_current'] = temp_current
 
         # WINDCHILL
 
         soup = BeautifulSoup(str(row_temp_windchill), 'html.parser')
         td = soup.find_all('td')
         temp_windchill = td[1].text
+        json_data['temp_windchill'] = temp_windchill
 
         # TEMP MAX TODAY
 
         soup = BeautifulSoup(str(row_temp_today_max), 'html.parser')
         td = soup.find_all('td')
         temp_today_max = td[1].text
+        json_data['temp_today_max'] = temp_today_max
 
         # TEMP MIN TODAY
 
         soup = BeautifulSoup(str(row_temp_today_min), 'html.parser')
         td = soup.find_all('td')
         temp_today_min = td[1].text
+        json_data['temp_today_min'] = temp_today_min
 
-        # Print all the things
-        print("\n\nTIME: %s" % time)
-        print("PRESSURE: %s" % current_pressure)
-        print("WIND: %s" % current_wind)
-        print("RAIN TODAY: %s" % rain_today)
-        print("RAIN_RECENTLY: %s" % rain_recent)
-        print("TEMP: %s" % temp_current)
-        print("WINDCHILL: %s" % temp_windchill)
-        print("TEMP MAX: %s" % temp_today_max)
-        print("TEMP MIN: %s" % temp_today_min)
+        return(json_data)
 
         #
         # We dont need this any more, for row debugging
         #
 
-#       for row in rows:
-#           print("%d: %s" % (count, row) )
-#           count = count +1
-#           rowtext = str(row)
-#           if rowtext.startswith('<tr>'):
-#           print(row)
+        #       for row in rows:
+        #           print("%d: %s" % (count, row) )
+        #           count = count +1
+        #           rowtext = str(row)
+        #           if rowtext.startswith('<tr>'):
+        #           print(row)
     
         return
 
@@ -120,11 +121,25 @@ class CleeveWeatherMonitor:
             while True:
                 
                 weather = self.getWeather()
+                
+                # Print all the things
+                print("\n\nTIME: %s" % weather['time'])
+                print("PRESSURE: %s" % weather['current_pressure'])
+                print("WIND: %s" % weather['current_wind'])
+                print("RAIN TODAY: %s" % weather['rain_today'])
+                print("RAIN_RECENTLY: %s" % weather['rain_recent'])
+                print("TEMP: %s" % weather['temp_current'])
+                print("WINDCHILL: %s" % weather['temp_windchill'])
+                print("TEMP MAX: %s" % weather['temp_today_max'])
+                print("TEMP MIN: %s" % weather['temp_today_min'])
+
                 time.sleep(10)
 
         except KeyboardInterrupt:
             print('interrupted!')
             print("")
+
+
 
 weatherMonitor = CleeveWeatherMonitor()
 weatherMonitor.watch()
